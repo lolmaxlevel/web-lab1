058040f95343c2eef5e05716aec851469bde537d
lolmaxlevel <lolmaxlevel@gmail.com>
2022-09-16 18:02:22 +0300

    all done

--- js/main.js
+++ js/main.js
@@ -1,47 +1,47 @@
 function isInt(value) {
   return !isNaN(value) &&
-      parseInt(Number(value)) == value &&
-      !isNaN(parseInt(value, 10));
+      parseInt(Number(value)) == value && !isNaN(parseInt(value, 10));
 }
 function isFloat(str) {
   if (typeof str != "string") return false
-  return !isNaN(str) &&
-      !isNaN(parseFloat(str))
+  return !isNaN(str) && !isNaN(parseFloat(str))
 }
-
+function sendRequest(data){
+  $.ajax({
+    url: 'php/main.php',
+    method: 'POST',
+    data: data,
+    dataType: "json",
+    success: function(data) {
+      newRow = '<tr>';
+      newRow += '<td>' + data.xval + '</td>';
+      newRow += '<td>' + data.yval + '</td>';
+      newRow += '<td>' + data.rval + '</td>';
+      newRow += '<td>' + data.curtime + '</td>';
+      newRow += '<td>' + data.exectime + '</td>';
+      let color = data.hitres ? "green":"red";
+      newRow += '<td>' + '<span class='+color+'>'+ data.hitres +'</span>'+ '</td>';
+      $('#result-table').append(newRow);
+    }
+  });
+}
   $('#example').on('click', function(e) {
     var pos = findPos(this);
     var x = e.pageX - pos.x;
     var y = e.pageY - pos.y;
     if (!validateR()) return;
     let r_val = document.getElementById('r-text-input').value
-    $.ajax({
-      url: 'php/main.php',
-      method: 'POST',
-      data: {
-        'type':'img',
-        'x_cord':x,
-        'y_cord':y,
-        'r':r_val,
-        'timezone': new Date().getTimezoneOffset()
-      },
-      dataType: "json",
-      beforeSend: function() {
-        $('.button').attr('disabled', 'disabled');
-      },
-      success: function(data) {
-        $('.button').attr('disabled', false);
-          newRow = '<tr>';
-          newRow += '<td>' + data.xval + '</td>';
-          newRow += '<td>' + data.yval + '</td>';
-          newRow += '<td>' + data.rval + '</td>';
-          newRow += '<td>' + data.curtime + '</td>';
-          newRow += '<td>' + data.exectime + '</td>';
-          newRow += '<td>' + data.hitres + '</td>';
-          $('#result-table').append(newRow);
-
-      }
-    });
+    let experimental = $('.specialCB:checked').val();
+    let normalized_x = ((x - 23 - 80) / 80) * r_val;
+    let normalized_y = -((y - 25 - 80) / 80) * r_val;
+    let data= {
+      'type':experimental? "experimental":"default",
+      'xval':experimental? x : normalized_x,
+      'yval':experimental? y : normalized_y,
+      'rval':r_val,
+      'timezone': new Date().getTimezoneOffset(),
+    }
+    sendRequest(data);
   });
 
   function findPos(obj) {
@@ -93,25 +93,6 @@ function isFloat(str) {
   $('#input-form').on('submit', function(event) {
     event.preventDefault();
     if (!validateForm()) return;
-    $.ajax({
-      url: 'php/main.php',
-      method: 'POST',
-      data: $(this).serialize() + '&timezone=' + new Date().getTimezoneOffset() + '&type=abob',
-      dataType: "json",
-      beforeSend: function() {
-        $('.button').attr('disabled', 'disabled');
-      },
-      success: function(data) {
-        $('.button').attr('disabled', false);
-          newRow = '<tr>';
-          newRow += '<td>' + data.xval + '</td>';
-          newRow += '<td>' + data.yval + '</td>';
-          newRow += '<td>' + data.rval + '</td>';
-          newRow += '<td>' + data.curtime + '</td>';
-          newRow += '<td>' + data.exectime + '</td>';
-          let color = data.hitres ? "green":"red";
-          newRow += '<td>' + '<span class='+color+'>'+ data.hitres +'</span>'+ '</td>';
-          $('#result-table').append(newRow);
-      }
-    });
+    let data = $(this).serialize() + '&timezone=' + new Date().getTimezoneOffset() + '&type=default';
+    sendRequest(data);
   });
--- php/main.php
+++ php/main.php
@@ -1,8 +1,8 @@
 <?php
 
-
 // Hit check functions
-function checkTriangle($xVal, $yVal, $rVal) {
+function checkTriangle($xVal, $yVal, $rVal): bool
+{
     return (($xVal) * ($rVal) <= 0 &&
             (-$xVal) * ($rVal) - ($rVal) * (-$rVal - $yVal) <= 0 &&
             -(($rVal) * ($yVal)) <=0) || (($xVal) * ($rVal) >= 0 &&
@@ -11,38 +11,41 @@ function checkTriangle($xVal, $yVal, $rVal) {
 
 }
 
-function checkRectangle($xVal, $yVal, $rVal) {
+function checkRectangle($xVal, $yVal, $rVal): bool
+{
   return $xVal <= 0 && $yVal <= 0 && abs($xVal) <= $rVal && abs($yVal) <= $rVal/2;
 }
 
-function checkCircle($xVal, $yVal, $rVal) {
+function checkCircle($xVal, $yVal, $rVal): bool
+{
   return $xVal >=0 && $yVal >= 0 && sqrt($xVal*$xVal + $yVal*$yVal) <= $rVal;
 }
 
-function checkHit($xVal, $yVal, $rVal) {
+function checkHit($xVal, $yVal, $rVal): bool
+{
   return checkTriangle($xVal, $yVal, $rVal) || checkRectangle($xVal, $yVal, $rVal) || checkCircle($xVal, $yVal, $rVal);
 }
+
 // Main
-if ($_POST['type'] == 'img'){
-    $xImg = $_POST['x_cord'];
-    $yImg = $_POST['y_cord'];
-    $img = imagecreatefrompng("graph.png");
-    $abob = imagecolorat($img, $xImg,$yImg);
+$type = $_POST['type'];
+$xVal = $_POST['xval'];
+$yVal = $_POST['yval'];
+$rVal = $_POST['rval'];
+if ($_POST['type'] == 'experimental'){
+    $img = imagecreatefrompng("../img/graph.png");
+    $abob = imagecolorat($img, $xVal,$yVal);
     $colors = imagecolorsforindex($img, $abob);
     $r = ($abob >> 16) & 0xFF;
-    $isHit = $r == 255 ? 1:0;
-    $xVal = "0";
-    $yVal = "0";
-    $rVal = "0";
+    $isHit = $r == 255 ? 0:1;
+    $xVal = "exp";
+    $yVal = "exp";
+    $rVal = "exp";
 }
 else{
-    $xVal = $_POST['xval'];
-    $yVal = $_POST['yval'];
-    $rVal = $_POST['rval'];
     $isHit = checkHit($xVal, $yVal, $rVal) ;
 }
 $timezoneOffset = $_POST['timezone'];
-$converted_isHit = $isHit ? 'false' : 'true';
+$converted_isHit = $isHit ? 'true' : 'false';
 $currentTime = date('H:i:s', time()-$timezoneOffset*60);
 $executionTime = microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"];
 $jsonData = '{' .
